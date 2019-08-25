87ec70de3784a3d415bef88641ec5d9bacef4a9a
ryuichi1208 <ryucrosskey@gmail.com>
2019-08-25 23:20:45 +0900

    add model

---
+++ model/Error.go
@@ -0,0 +1,6 @@
+package model
+
+type Error struct {
+	Error string
+	Code  int
+}
---
+++ model/User.go
@@ -0,0 +1,8 @@
+package model
+
+type User struct {
+	Id      string `json:"userId"`
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string `json:"address"`
+    }
